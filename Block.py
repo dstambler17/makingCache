70084f1e33a33c96013f9fd0d9ec34044bd9df83
Daniel <dstambl2@jhu.edu>
2018-04-19 17:23:25 -0400

    created block object

---
+++ Block.py
@@ -0,0 +1,19 @@
+class Block:
+    tag = 0
+    dirty_bit = 0
+    time_in_cell = 0
+
+    def __init__(self, tag):
+        self.tag = tag
+
+    def set_dirty_bit_true(self):
+        self.dirty_bit = 1
+
+    def get_dirty_bit(self):
+        return self.dirty_bit
+
+    def increment_time(self):
+        self.time_in_cell = self.time_in_cell + 1
+
+    def get_tag(self):
+        return self.tag
